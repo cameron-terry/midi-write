9fd87935697647a1c72667c3fa1092b899aeea83
Beurkum <cwterry314@gmail.com>
2018-05-29 07:28:30 -0400

    Added markup file and custom file functionality

--- midi_write.py
+++ midi_write.py
@@ -2,36 +2,25 @@
 # TODO: abstract musical ideas even further
 #       e.g. add support for cycle of fifths, hammer-ons / pull-offs, and more
 # TODO: add variability to note length
-# TODO: add file for defining new chords (e.g. x2222x = B9sus4)
-#       if it doesn't find chord in definition look in file
-#       or denote with character %
-#TODO: add arpeggios! and markup file
+# TODO: add arpeggios! and markup file
 #       arpeggios shouldn't be too hard. it's a matter of spacing out the notes in the chord (increasing delta-time)
-#       sample markup file:
-#       e.g.
-#           <begin [title]>
-#               <time-sig=[time_sig]> (not a necessary tag, default is 4/4)
-#               <tempo=[tempo]> (not a necessary tag, default is 120
-#               <key_sig=[key_sig]> (not a necessary tag, default is Cmaj)
-#               <begin commands>
-#                   commands = [<command1> <command2> ... ]
-#               <end commands>
-#           <end [title]>
-#
-#       and the output, of course, is a midi file.
 ##-------------------------------------------------------------------------------------------------------------------##
 # MidiWrite by Cameron Terry
 # May 28, 2018
-# time: 2 days (and counting!)
 #
-# MidiWrite makes turning a chord progression into a midi file a super simple process while lessening the time
-# required needed to create a midi file based on a progression.
+# MidiWrite makes turning a chord progression into a MIDI file a super simple process while lessening the time
+# required needed to create a MIDI file based on a progression.
 ##-------------------------------------------------------------------------------------------------------------------##
 
 import array
 
 
 class MidiWrite:
+    # user defined file that contains additional chord mappings
+    custom_write = None
+
+    debug = False  # set in track_chunk
+
     # constant bytes
     mthd                = b'\x4d\x54\x68\x64'
     header_chunk_length = b'\x00\x00\x00\x06'
@@ -57,6 +46,54 @@ class MidiWrite:
         "E": ["E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb"]
     }
 
+    # used for translating chords to notes
+    chord_dict = {
+        "dim7**": [0, 9, 5, 18],
+        "dim7*":  [0, 6, 10, 13, 19],
+        "aug7**": [0, 11, 16, 20],
+        "aug7*":  [0, 8, 11, 16],
+        "dim**":  [0, 6, 12, 15],
+        "dim*":   [0, 6, 9, 15],
+        "aug**":  [0, 4, 8, 12, 16],
+        "aug*":   [0, 4, 8, 12],
+        "sus4**": [0, 7, 12, 17, 19, 24],
+        "sus4*":  [0, 7, 12, 17, 19],
+        "sus2**": [0, 14, 19, 24],
+        "sus2*":  [0, 7, 12, 14, 19],
+        "Mm7**":  [0, 11, 15, 19],
+        "Mm7*":   [0, 7, 11, 15, 21],
+        "m7b5**": [0, 10, 15, 18],
+        "m7b5*":  [0, 6, 10, 15],
+        "maj6**": [0, 9, 16, 19],
+        "maj6*":  [0, 7, 12, 16, 21],
+        "m6**":   [0, 9, 15, 19],
+        "m6*":    [0, 7, 12, 15, 21],
+        "maj7**": [0, 11, 16, 19],
+        "maj7*":  [0, 7, 11, 16, 19],
+        "maj**":  [0, 7, 12, 16, 19, 24],
+        "maj*":   [0, 7, 12, 16, 19],
+        "m7**":   [0, 10, 15, 19],
+        "m7*":    [0, 7, 10, 15, 19],
+        "m**":    [0, 7, 12, 15, 19, 24],
+        "m*":     [0, 7, 12, 15, 19],
+        "7**":    [0, 10, 15, 19],
+        "7*":     [0, 7, 10, 16, 19],
+        "13**":   [0, 10, 16, 21],
+        "13*":    [0, 4, 10, 14, 21]
+    }
+
+    @staticmethod
+    def is_number(s):
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
+
+    @staticmethod
+    def set_custom_file(file: str):
+        if file is not None:
+            MidiWrite.custom_file = file
 
     @staticmethod
     def get_key(k: str):
@@ -119,6 +156,8 @@ class MidiWrite:
     # based on pseudo-code from http://midi.teragonaudio.com/tech/midifile/vari.htm
     @staticmethod
     def write_var_len(n: int) -> [int]:
+        EIGHT_BIT_MAX = 256
+        SEVEN_BIT_MAX = 128
         '''
         Transforms an integer into a variable-length quantity
         :param n: the integer to convert
@@ -128,7 +167,7 @@ class MidiWrite:
         count = 0
 
         while True:
-            if n < 128:
+            if n < SEVEN_BIT_MAX:
                 byte_arr[count] = n & 0x7f
                 count += 1
                 break
@@ -144,7 +183,7 @@ class MidiWrite:
         r = True  # used for additional formatting
         for i in range(len(byte_arr)):
             if i < len(byte_arr) - 1:
-                if byte_arr[i] + byte_arr[i + 1] < 256:
+                if byte_arr[i] + byte_arr[i + 1] < EIGHT_BIT_MAX:
                     byte_arr[i] = byte_arr[i] + byte_arr[i + 1]
                     byte_arr[i + 1] = 0
                     r = False
@@ -165,7 +204,7 @@ class MidiWrite:
         result = 0
         for val in n:
             result <<= 7
-            result |= (val & 0x7F)
+            result |= (val & 0x7f)
             if not (val & 0x80 == 0x80):
                 return result
 
@@ -176,6 +215,11 @@ class MidiWrite:
         :param file: the midi file to write to
         :return: none
         '''
+        # check prefix
+        if file[-5:] != ".midi":
+            print("File not could be created.")
+            exit(1)
+
         MidiWrite.write_header_chunk(file)
         MidiWrite.write_track_chunk(file)
 
@@ -186,9 +230,9 @@ class MidiWrite:
         :param file: the midi file to write to
         :return: none
         '''
-        fmat         = b'\x00\x01'
-        track        = b'\x00\x02'
-        quart_length = b'\x00\x60'
+        fmat         = b'\x00\x01' # multiple-track format
+        track        = b'\x00\x02' # 1 track
+        quart_length = b'\x00\x60' # quarter tick length
 
         with open(file, "wb") as f:
             f.write(MidiWrite.mthd)
@@ -228,32 +272,41 @@ class MidiWrite:
             f.write(eot)
 
     @staticmethod
-    def write_track(file: str, commands: [bytes], title='Main', key='Cmaj', debug=False):
+    def write_track(file: str, commands: [bytes], title='Main', time="4/4", tempo=60, key='Cmaj', debug=False):
+        if debug:
+            MidiWrite.debug = True
         '''
         write the track data to the midi file
         :param file: the midi file to write to
         :param commands: the notes to write to the midi file
         :param title: the title of the track
         :param key: the key signature of the track
+        :param debug: show progress on creating midi file
         :return:
         '''
         chunk_length = b'\x00\x00'
         preset = b'\x00\xc1' + bytes([24])
         chunk_title = b'\x00\xff\x03'
         key_sig = b'\x00\xff\x59\x02'
+        time_sig = b'\x00\xff\x58\x04'
+        time_sig_end_bytes = b'\x24\x08'
 
         chunk_title_length = bytes([len(title)])
         chunk_title += chunk_title_length
         for letter in title:
             chunk_title += bytes([ord(letter)])
 
-
         flats, major_minor = MidiWrite.get_key(key)
+
+        ts_num = bytes([int(time.split("/")[0])])
+        ts_denom = bytes([int(time.split("/")[1])])
+
+        time_sig += ts_num + ts_denom + time_sig_end_bytes
 
         key_sig += bytes([flats])
         key_sig += bytes([major_minor])
 
-        count = 7 + len(chunk_title) + len(key_sig)
+        count = len(chunk_length) + len(MidiWrite.eof) + len(chunk_title) + len(key_sig) + len(time_sig)
         notes_list = []
 
         for chord in commands:
@@ -277,11 +330,11 @@ class MidiWrite:
             f.write(preset)
 
             for i in range(len(notes_list)):
-                if debug:
-                    print("Writing %s to %s... " % (commands[i], file), end="", flush=True)
+                if MidiWrite.debug:
+                    print("Writing \"%s\" to %s... " % (commands[i], file), end="", flush=True)
                 for note in notes_list[i]:
                     f.write(note)
-                if debug:
+                if MidiWrite.debug:
                     print("Done.")
 
             f.write(MidiWrite.eof)
@@ -326,52 +379,67 @@ class MidiWrite:
         :return: the chord as a set of integer notes
         '''
         if isinstance(chord, str):
+            # look for note and chord type in dictionaries
             for element in MidiWrite.note_map:
                 if element in chord:
                     base = MidiWrite.note_map[element]
-                    if "maj7" in chord:
-                        if chord[-1] == "*":
-                            if chord[-2] == "*":
-                                return [base, base + 11, base + 16, base + 19]
-                            else:
-                                return [base, base + 7, base + 11, base + 16, base + 19]
+                    if '%' not in chord:
+                        for c_shape in MidiWrite.chord_dict:
+                            if c_shape in chord:
+                                return [base + i for i in MidiWrite.chord_dict[c_shape]]
+                    else:
+                        # look for chord in separate custom file
+                        # custom file defines chords like so: <chord> : <[notes]> or <chord>:<fret notation>
+                        # TODO: currently can only define one type of custom chord, edit to allow multiple
+                        # e.g. F7%, F7%%, etc
+                        custom_dict = {}
+                        if MidiWrite.custom_file is not None:
+                            with open(MidiWrite.custom_file, 'r') as f:
+                                for line in f:
+                                    definition = line.split(":")
 
-                    elif "m7" in chord:
-                        if chord[-1] == "*":
-                            if chord[-2] == "*":
-                                return [base, base + 10, base + 15, base + 19]
-                            else:
-                                return [base, base + 7, base + 10, base + 15, base + 19]
+                                    if 'x' in definition[1][0]:  # fret-notation
+                                        chord = definition[1]
+                                        break
 
-                    elif "7" in chord:
-                        if chord[-1] == "*":
-                            if chord[-2] == "*":
-                                return [base, base + 10, base + 15, base + 19]
-                            else:
-                                return [base, base + 7, base + 10, base + 16, base + 19]
-                        elif chord[-1] == "%":
-                            return [base, base + 10, base + 15, base + 19]
+                                    definition[1] = [int(j) for j in definition[1].split(",")]
+                                    custom_dict[definition[0]] = definition[1]
+                            for custom_c_shape in custom_dict:
+                                if custom_c_shape in chord:
+                                    return [base + i for i in custom_dict[custom_c_shape]]
 
-                    elif "13" in chord:
-                        if chord[-1] == "*":
-                            if chord[-2] == "*":
-                                return [base, base + 10, base + 16, base + 21]
-                            else:
-                                return [base, base + 4, base + 10, base + 14, base + 21]
-
+        # assume chord is in fret-notation
         notes = []
-        if chord[0] != 'x':
-            notes.append(MidiWrite.guitar_map_standard_tuning["E"][int(chord[0])])
-        if chord[1] != 'x':
-            notes.append(MidiWrite.guitar_map_standard_tuning["A"][int(chord[1])])
-        if chord[2] != 'x':
-            notes.append(MidiWrite.guitar_map_standard_tuning["D"][int(chord[2])])
-        if chord[3] != 'x':
-            notes.append(MidiWrite.guitar_map_standard_tuning["G"][int(chord[3])])
-        if chord[4] != 'x':
-            notes.append(MidiWrite.guitar_map_standard_tuning["B"][int(chord[4])])
-        if chord[5] != 'x':
-            notes.append(MidiWrite.guitar_map_standard_tuning["E"][int(chord[5])])
+        if MidiWrite.is_number(chord[0]):
+            notes.append(MidiWrite.guitar_map_standard_tuning["E"][int(chord[0]) % 12])  # octaves repeat
+        else:
+            if MidiWrite.debug and chord[0] != 'x':
+                print("Invalid character found, is ignored.")
+        if MidiWrite.is_number(chord[1]):
+            notes.append(MidiWrite.guitar_map_standard_tuning["A"][int(chord[1]) % 12])
+        else:
+            if MidiWrite.debug and chord[1] != 'x':
+                print("Invalid character found, is ignored.")
+        if MidiWrite.is_number(chord[2]):
+            notes.append(MidiWrite.guitar_map_standard_tuning["D"][int(chord[2]) % 12])
+        else:
+            if MidiWrite.debug and chord[2] != 'x':
+                print("Invalid character found, is ignored.")
+        if MidiWrite.is_number(chord[3]):
+            notes.append(MidiWrite.guitar_map_standard_tuning["G"][int(chord[3]) % 12])
+        else:
+            if MidiWrite.debug and chord[3] != 'x':
+                print("Invalid character found, is ignored.")
+        if MidiWrite.is_number(chord[4]):
+            notes.append(MidiWrite.guitar_map_standard_tuning["B"][int(chord[4]) % 12])
+        else:
+            if MidiWrite.debug and chord[4] != 'x':
+                print("Invalid character found, is ignored.")
+        if MidiWrite.is_number(chord[5]):
+            notes.append(MidiWrite.guitar_map_standard_tuning["E"][int(chord[5]) % 12])
+        else:
+            if MidiWrite.debug and chord[5] != 'x':
+                print("Invalid character found, is ignored.")
 
         for i in range(len(notes)):
             if len(notes[i]) > 2:
