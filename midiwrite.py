d01ce83232b8bfbfaf9a6142990296c6559ba056
Beurkum <cwterry314@gmail.com>
2018-05-29 18:26:52 -0400

    Changed parser functionality

--- midiwrite.py
+++ midiwrite.py
@@ -1,23 +1,27 @@
 # markup file parser for MidiWrite
 
 import sys
-from midi_write import MidiWrite
+from midi_writer import MidiWrite
 
 if __name__ == "__main__":
     file = sys.argv[1]
     output_file = file[:-4] + ".midi"
     custom_file = None
     title = None
+    tempo = None
     time_sig = None
     key_sig = None
     command_listing = False
     commands = []
+
+    was_prefix = False
 
     if len(sys.argv) > 2:
         octave_shift = int(sys.argv[2])
     else:
         octave_shift = None
 
+    i = 0
     with open(file, 'r') as f:
         first_line = True
         prefix = False
@@ -25,7 +29,7 @@ if __name__ == "__main__":
             line = line.strip()[1:-1]
             if first_line:
                 if not line.startswith("begin"):
-                    print("Error: file does not start with 'begin'.")
+                    print("Error in [%s]: file does not start with 'begin' [line: %d]" % (file, i + 1))
                     exit(1)
                 else:
                     title = line.split(" ")[1]
@@ -36,20 +40,20 @@ if __name__ == "__main__":
                 else:
                     commands += line.replace("\"", "").replace(" ", "").split(',')
                     if line.startswith("end"):
-                        print("Error: Commands not finished.")
+                        print("Error in [%s]: Commands not finished [line: %d]" % (file, i + 1))
 
             if prefix:
                 if line.startswith("time-sig"):
                     time_sig = line.split("=")[1]
                 elif line.startswith("tempo"):
-                    tempo = line.split("=")[1]
+                    tempo = int(line.split("=")[1])
                 elif line.startswith("key-sig"):
                     key_sig = line.split("=")[1]
                 elif line == "/prefix":
                     if prefix:
                         prefix = False
                 else:
-                    print("Error: variable declaration outside of prefix.")
+                    print("Error in [%s]: variable declaration outside of prefix [line: %d]" % (file, i + 1))
                     exit(1)
             else:
                 if line.startswith("custom_file"):
@@ -60,32 +64,43 @@ if __name__ == "__main__":
 
             if line == "prefix":
                 prefix = True
+                was_prefix = True
 
             first_line = False
 
-    if octave_shift is not None:
-        if octave_shift < 0:
-            MidiWrite.octave_shift_down(octave_shift)
-        else:
-            MidiWrite.octave_shift_up(octave_shift)
+            if line.startswith("end"):
+                if title != line.split(" ")[1]:
+                    print("Error in [%s]: beginning and end tags do not match titles [line: %d]" % (file, i + 1))
+                    exit(1)
+
+            i += 1
 
     MidiWrite.set_custom_file(custom_file)
-    MidiWrite.write_preqs(output_file)
+
+    if was_prefix:
+        if time_sig is not None and tempo is None:
+            MidiWrite.write_preqs(output_file, time=time_sig)
+        elif time_sig is None and tempo is not None:
+            MidiWrite.write_preqs(output_file, tempo=tempo)
+        else:
+            MidiWrite.write_preqs(output_file, time=time_sig, tempo=tempo)
+    else:
+        MidiWrite.write_preqs(output_file)
 
     if time_sig is None:
         if tempo is not None and key_sig is None:
-            MidiWrite.write_track(output_file, commands, title=title, tempo=tempo)
+            MidiWrite.write_track(output_file, commands, title=title, shift=octave_shift)
         elif tempo is None and key_sig is not None:
-            MidiWrite.write_track(output_file, commands, title=title, key=key_sig)
+            MidiWrite.write_track(output_file, commands, title=title, key=key_sig, shift=octave_shift)
     elif tempo is None:
         if key_sig is None:
-            MidiWrite.write_track(output_file, commands, title=title, time=time_sig)
+            MidiWrite.write_track(output_file, commands, title=title, shift=octave_shift)
         else:
-            MidiWrite.write_track(output_file, commands, title=title, time=time_sig, key=key_sig)
+            MidiWrite.write_track(output_file, commands, title=title, key=key_sig, shift=octave_shift)
     elif key_sig is None:
         if tempo is None:
-            MidiWrite.write_track(output_file, commands, title=title, time=time_sig, key=key_sig)
+            MidiWrite.write_track(output_file, commands, title=title, key=key_sig, shift=octave_shift)
         else:
-            MidiWrite.write_track(output_file, commands, title=title, time=time_sig, tempo=tempo)
+            MidiWrite.write_track(output_file, commands, title=title, shift=octave_shift)
     else:
-        MidiWrite.write_track(output_file, commands, title=title, time=time_sig, tempo=tempo, key=key_sig)
+        MidiWrite.write_track(output_file, commands, title=title, key=key_sig, shift=octave_shift)
